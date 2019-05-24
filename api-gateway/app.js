35cf718e789e1225546df41bdac0c966dfb17709
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-05-24 13:05:00 -0400

    bcrypt not working

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -15,9 +15,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var apiCatalog = require('./routes/api-catalog');
 
+var apiCatalog = require('./routes/api-catalog');
 var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use('/', indexRouter);
+app.use('/api', apiCatalog); 
 
 // Connect to DB
 const mongoDB = "mongodb+srv://admin:admin@ems-qmc0r.mongodb.net/api-gateway?retryWrites=true";
@@ -30,20 +35,18 @@ db.once("open", function() {
   console.log("Application connected to Atlas MongoDB instance");
 });
 
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/api', apiCatalog); 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', apiCatalog); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
