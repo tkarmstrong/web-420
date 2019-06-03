b2e1aeb6f25dd4376343ad757df3230963105b48
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-06-02 21:34:14 -0400

    Corrected .findByID to .findById

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -28,5 +28,5 @@ module.exports.add = (user, callback) => {
 //
 module.exports.getById = (id, callback) => {
   var query = {_id: id};
-  User.findByID(query, callback);
+  User.findById(query, callback);
 }
